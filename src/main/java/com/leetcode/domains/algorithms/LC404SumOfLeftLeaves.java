dd421f8debd35defc9b91e3045d5557d4e996e27
Michael Laucella <michael.laucella@ipsoft.com>
2018-04-03 10:19:08 -0400

    LC404 solved

---
+++ src/main/java/com/leetcode/domains/algorithms/LC404SumOfLeftLeaves.java
@@ -0,0 +1,35 @@
+
+package com.leetcode.domains.algorithms;
+
+import com.leetcode.io.ArgumentParser;
+import com.leetcode.io.Display;
+import com.leetcode.util.TreeNode;
+import com.leetcode.util.parsers.string.IntParser;
+import com.leetcode.util.parsers.string.StringParser;
+
+public class LC404SumOfLeftLeaves {
+
+    public static void main(String[] args) {
+        String arg = ArgumentParser.parseArg(args, 0, new StringParser());
+        TreeNode<Integer> tree = ArgumentParser.<Integer>parseTree(arg, new IntParser());
+
+        Display.printTree(tree);
+        System.out.println(new Solution().sumOfLeftLeaves(tree));
+    }
+
+    static class Solution {
+
+        public int leftTraverse(TreeNode<Integer> root) {
+            if (root == null) return 0;
+            else if (root.left == null && root.right == null) return root.val;
+            return sumOfLeftLeaves(root.right) + leftTraverse(root.left);
+        }
+        
+        public int sumOfLeftLeaves(TreeNode<Integer> root) {
+            if (root == null) return 0;
+            return sumOfLeftLeaves(root.right) + leftTraverse(root.left);
+        }
+
+    }
+
+}
